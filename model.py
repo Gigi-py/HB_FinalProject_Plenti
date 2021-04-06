ed2bdfd1e67f0ee7e11cf42e4b4c13b3b1531ad5
Gabriella Savage <gabriella.h.savage@gmail.com>
2021-04-06 21:58:06 +0000

    continuing on data model

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
+
 from datetime import datetime
 #add datatime library
 from flask_sqlalchemy import SQLAlchemy
 #add SQLAlchemy functionality
+import json
 
 db = SQLAlchemy()
 
@@ -12,13 +14,14 @@ class User(db.Model):
 __tablename__ ='users'
 
     user_id = db.Column(db.Integer, autoincrement= True, primary_key= True)
+    username = db.Column(db.String)
     email = db.Column(db.String, unique = True)
-    password= db.Column(db.String)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
+    password = db.Column(db.String)
+    fname = db.Column(db.String)
+    lname = db.Column(db.String)
 
     def __repr__(self):
-        return f'<User user_id={self.user_id} email={self.email}>'
+        return f'<User {self.user_id} {self.email}>'
 
 
 class Stock(db.Model):
@@ -33,32 +36,29 @@ class Stock(db.Model):
     company_name = db.Column(db.String, nullable = False)
     # exchange = db.Column(db.String, nullable = False)
     asset_type = db.Column(db.String, nullable = False)
-    status = db.Column(db.String, nullable = False)
+    ipo_date = db.Column(db.datetime, nullable = False)
     current_price = db.Column(db.Integer, nullable = False)
     ipo_price = db.Column(db.Integer, nullable = False)
     # ipo_date = db.Column(db.String, nullable = False)
     # delisting_date = db.Column(db.String, nullable = False)
-    women_lead = db.Column(db.Boolean, default = False )
 
     def __repr__(self):
-        return f'<Stock stock_id={self.stock_id} symbol={self.symbol}>'
+        return f'<Stock {self.stock_id} {self.symbol}>'
 
 
-class UserBasket(db.Model):
+class Subscription(db.Model):
     """users items in subscription box """
 
-    __tablename__ = 'userBasket'
+    __tablename__ = 'subscription'
 
-    basket_id = db.Column(db.Integer, autoincrement= True, primary_key=True)
-    is_favorite = db.Column(db.Boolean, default = True)
+    subscription_id = db.Column(db.Integer, autoincrement= True, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    stock_id = db.Column(db.Integer, db.ForeignKey('stocks.stock_id'))
    
-    stock = db.relationship('Stock', backref = 'userBasket')
-    user = db.relationship('User', backref = 'userBasket')
+    stock = db.relationship('Stock', backref = 'subscription')
+    user = db.relationship('User', backref = 'subscription')
 
     def __repr__(self):
-        return f'<userBasket favorite_id={self.basket_id} is_favorite={self.is_favorite}>'
+        return f'<Subscription {self.subscription_id} by {self.is_favorite}>'
 
 
 if __name__ == '__main__':
--- server.py
+++ server.py
@@ -1,12 +1,30 @@
-from flask import (Flask, render_template, request, flash, session,
+from flask import (Flask, render_template, request, flash, session, jsonify
                    redirect)
 from model import connect_to_db
 import crud
-
+import math
+from flask_sqlalchemy import SQLAlchemy
 from jinja2 import StrictUndefined
 
 
+ALPHAVANTAGE_KEY = os.environ('ALPHAVANTAGE_KEY')
+
+cloudinary.config(
+  cloud_name = os.environ['CLOUDINARY_CLOUD_NAME'],  
+  api_key = os.environ['CLOUDINARY_API_KEY'],  
+  api_secret = os.environ['CLOUDINARY_API_SECRET'] 
+)
+
 app = Flask(__name__)
+app.secret_key = 'dev'
+
+@app.route('/', defaults={'path': ''}) 
+@app.route('/<path:path>') 
+def show_homepage(path):
+    """View the homepage."""
+    
+    return render_template('index.html')
+
 
 
 # Replace this with routes and view functions!
