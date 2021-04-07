e9d331a509a04f0979244c58ba365c9111542ccb
Gabriella Savage <gabriella.h.savage@gmail.com>
2021-04-07 05:53:14 +0000

    completed initial model.py

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
+"""data models for Plenti"""
 
 from datetime import datetime
 #add datatime library
 from flask_sqlalchemy import SQLAlchemy
 #add SQLAlchemy functionality
+from werkzeug.security import generate_password_hash, check_password_hash
 import json
 
 db = SQLAlchemy()
@@ -13,52 +15,159 @@ class User(db.Model):
 
 __tablename__ ='users'
 
-    user_id = db.Column(db.Integer, autoincrement= True, primary_key= True)
-    username = db.Column(db.String)
-    email = db.Column(db.String, unique = True)
-    password = db.Column(db.String)
+    id = db.Column(db.Integer, autoincrement=True, 
+                    primary_key=True)
+    username = db.Column(db.String) #unique=True
     fname = db.Column(db.String)
     lname = db.Column(db.String)
+    email = db.Column(db.String, unique=True)
+    password_hash = db.Column(db.String)
+    image_url = db.Column(db.String, default='')
+    about = db.Column(db.Text)
 
     def __repr__(self):
-        return f'<User {self.user_id} {self.email}>'
+        return f'<User {self.fname} {self.lname}>'
 
+    def to_dict(self, include_email=False):
+        data = {
+            'id': self.id,
+            'username': self.username,
+            'fname': self.fname,
+            'lname': self.lname,
+            'image_url': self.image_url,
+            'about': self.about
+        }
+        if include_email:
+            data['email'] = self.email
+        return data
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    """Stocks Table"""
 
 class Stock(db.Model):
-    """All Stocks"""
-    """needs to dig into the API"""
+    #todo: check API to see what data I am able to get back
 
     __tablename__ = 'stocks'
 
-
     stock_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     symbol = db.Column(db.String, nullable = False)
-    company_name = db.Column(db.String, nullable = False)
-    # exchange = db.Column(db.String, nullable = False)
+    company_overview = db.Column(db.String, nullable = False)
+    industry_type = db.Column(db.String, nullable = False)
     asset_type = db.Column(db.String, nullable = False)
     ipo_date = db.Column(db.datetime, nullable = False)
     current_price = db.Column(db.Integer, nullable = False)
     ipo_price = db.Column(db.Integer, nullable = False)
     # ipo_date = db.Column(db.String, nullable = False)
-    # delisting_date = db.Column(db.String, nullable = False)
 
     def __repr__(self):
         return f'<Stock {self.stock_id} {self.symbol}>'
 
+    def to_dict(self):
+        data = {
+            'stock_id': self.id,
+            'symbol': self.symbol,
+            'company_overview': self.company_overview
+            'industry_type': self.industry_type
+            'asset_type': = self.asset_type
+            'ipo_date': self.ipo_date
+            'current_price': self.current_price
+            'ipo_price': SELF.ipo_price
+        }
+        return data
+
 
 class Subscription(db.Model):
-    """users items in subscription box """
+    """different subscription options for users"""
 
-    __tablename__ = 'subscription'
+    __tablename__ = 'subscriptions'
 
     subscription_id = db.Column(db.Integer, autoincrement= True, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-   
-    stock = db.relationship('Stock', backref = 'subscription')
+    description = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id')) 
+    created_time = db.Column(db.datetime)
+    status = db.Column(db.String(10), default='ACTIVE')
+    subscription_value = db.Column(db.Integer) 
     user = db.relationship('User', backref = 'subscription')
 
     def __repr__(self):
-        return f'<Subscription {self.subscription_id} by {self.is_favorite}>'
+        return f'<Subscription {self.subscription_id} for {self.user}>'
+
+    def to_dict(self):
+        data = {
+            'subscription_id': self.subscription_id,
+            'description': self.description,
+            'user_id': self.user_id
+            'created_time': self.created_time
+            'status': self.status,
+            'capacity': self.capacity,
+            'subscription_value': self.subscription_value,
+        }
+        return data
+
+
+class Add_To_Subscription(db.Model):
+    """User adding a stock to a subcription box"""
+
+    __tablename__ = 'bridge_table'
+
+    event_id = db.Column(db.Integer, autoincrement= True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id')) 
+    stock_id = db.Column(db.Integer, db.ForeignKey('stocks.stock_id')) 
+    created_time = db.Column(db.datetime)
+    stock_price = db.Column(db.Integer, nullable = False) 
+    user = db.relationship('User', backref = 'Add_To_Subscription')
+    stock = db.relationship('Stock', backref = 'Add_To_Subscription')
+
+    def __repr__(self):
+        return f'<Added to subscription {self.event_id} of {self.stock_id} by {self.user}>'
+
+    def to_dict(self):
+        data = {
+            'event_id': self.event_id,
+            'user_id': self.user_id
+            'created_time': self.created_time
+            'stock_id': self.stock_id,
+            'stock_price': self.stock_price,
+        }
+        return data
+
+class Favorites(db.Model):
+    """favourite stocks by user"""
+
+    __tablename__ = 'favorites'
+
+    event_id = db.Column(db.Integer, autoincrement= True, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id')) 
+    status = db.Column(db.String(10), default='LIKED')
+    stock_id = db.Column(db.Integer, db.ForeignKey('stocks.stock_id')) 
+
+    user = db.relationship('User', backref = 'favorites')
+    stock = db.relationship('Stock', backref = 'favorites')
+
+    def __repr__(self):
+        return f'<Loved {self.event_id} by {self.user}>'
+
+    def to_dict(self):
+        data = {
+            'event_id': self.event_id,
+            'user_id': self.user_id
+            'status': self.status,
+            'stock_id': self.stock_id
+        }
+        return data
+
+def connect_to_db(flask_app, db_uri='postgresql:///', echo=False):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
 
 
 if __name__ == '__main__':
