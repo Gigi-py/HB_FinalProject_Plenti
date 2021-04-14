76c80302c8d8ec84de1c2c7a4a0426cea071e948
Gabriella Savage <gabriella.h.savage@gmail.com>
2021-04-14 04:57:27 +0000

    built Stripe checkout form

--- server.py
+++ server.py
@@ -9,6 +9,10 @@ import os
 import math
 from flask_sqlalchemy import SQLAlchemy
 from jinja2 import StrictUndefined
+import stripe
+
+stripe.api_key = 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+
 
 app = Flask(__name__)
 app.secret_key = 'dev'
@@ -81,8 +85,38 @@ def view_all_subscriptions():
 
 @app.route('/checkout')
 def checkout():
+    """view a list of all subscriptions to choose from."""
+    return render_template("/checkout.html") 
+
+@app.route('/create-checkout-session', methods=['POST']) 
+def create_checkout_session():
     """checkout subscription payments"""
-    return render_template("/checkout.html")
+    # This is a sample test API key. Sign in to see examples pre-filled with your key.
+    YOUR_DOMAIN = 'http://localhost:5000'
+    try:
+        checkout_session = stripe.checkout.Session.create(
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'currency': 'usd',
+                        'unit_amount': 2000,
+                        'product_data': {
+                            'name': 'Bronze Membership',
+                            'images': ['https://i.imgur.com/EHyR2nP.png'],
+                        },
+                    },
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=YOUR_DOMAIN + '/success.html',
+            cancel_url=YOUR_DOMAIN + '/cancel.html',
+        )
+        return jsonify({'id': checkout_session.id})
+    except Exception as e:
+        return jsonify(error=str(e)), 403
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
